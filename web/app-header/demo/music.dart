8d53eb1a38ff76d87b1ffe0abac25d267824e46b
ilikerobots <mike@hoolehan.com>
2016-05-09 22:07:10 +0300

    Use polymer-app-layout 0.0.3

--- web/app-header/demo/music.dart
+++ web/app-header/demo/music.dart
@@ -11,7 +11,6 @@ import 'package:polymer_elements/paper_fab.dart';
 import 'package:polymer_app_layout/app_toolbar.dart';
 import 'package:polymer_app_layout/app_box.dart';
 import 'package:polymer_app_layout/app_scroll_effects.dart';
-import 'package:polymer_app_layout/app_scroll_effects/effects/resize_snapped_title.dart'; //will be included in app_scroll_effects >= 0.0.16 of app-layout
 import 'package:polymer_app_layout/app_header.dart';
 
 import 'package:polymer_app_layout_demos/demo/sample_content.dart';
